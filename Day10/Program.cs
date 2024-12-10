7f4b6b6ecc36b3e183af1f8d55434531d70aeb3d
Thomas Dorfer <thomas.dorfer@ktm.at>
2024-12-10 23:32:09 +0100

    add solution for day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+
+const string inputFile = @"input.txt";
+var lines = await File.ReadAllLinesAsync(inputFile);
+
+var input = lines .Select(line => line.Select(c => c.ToInt()).ToArray()).ToArray();
+
+var startingPoints = input.Points().Where(point => input.GetValue(point) == 0);
+
+var trailHeads = startingPoints
+    .Select(start => (start, Hike(input, start, -1)))
+    .ToDictionary();
+
+var result1 = trailHeads.Values.Select(top => top.Distinct().Count()).Sum();
+Console.WriteLine(result1);
+
+var result2 = trailHeads.Values.Select(top => top.Count()).Sum();
+Console.WriteLine(result2);
+
+Console.WriteLine("done");
+
+IEnumerable<Point> Hike(int[][] matrix, Point start, int lastValue)
+{
+    var currentValue = matrix.GetValue(start);
+
+    if (currentValue - lastValue != 1)
+    {
+        return [];
+    }
+
+    if (currentValue == 9)
+    {
+        return [start];
+    }
+
+    return new[] { start.Add(0, 1), start.Add(0, -1), start.Add(1, 0), start.Add(-1, 0) }
+        .Where(matrix.Contains)
+        .SelectMany(step => Hike(matrix, step, currentValue));
+}
