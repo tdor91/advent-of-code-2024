4984c54cbb03418159d5e2983a65dc4a5418b0aa
tdor91 <thomas.dorfer@gmx.net>
2024-12-19 19:08:24 +0100

    add solution for day 18

--- Common/Point.cs
+++ Common/Point.cs
@@ -2,6 +2,12 @@
 
 public readonly record struct Point(int X, int Y)
 {
+    public static Point Parse(string str)
+    {
+        var parts = str.Split(',');
+        return new(int.Parse(parts[0]), int.Parse(parts[1]));
+    }
+
     public static Point operator +(Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
     public static Point operator -(Point a, Point b) => new(a.X - b.X, a.Y - b.Y);
     public static Point operator *(Point a, int b) => new(a.X * b, a.Y * b);
---
+++ Day18/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using Common;
+
+const int size = 70;
+const string inputFile = @"input.txt";
+var lines = await File.ReadAllLinesAsync(inputFile);
+var obstacles = lines.Select(Point.Parse).ToArray();
+
+var start = new Point(0, 0);
+var end = new Point(size, size);
+
+var (_, endNode) = FindShortestPath(start, end, obstacles.Take(1024).ToArray(), size);
+var result1 = endNode.AccumulatedCost;
+Console.WriteLine(result1);
+
+var lastPossibleIndex = 0;
+var range = 0..(obstacles.Length - 1);
+int from = 0, to = obstacles.Length;
+while (from < to)
+{
+    int index = (from + to) / 2;
+    var currentObstacles = obstacles.Take(index).ToArray();
+    var (foundPath, _) = FindShortestPath(start, end, currentObstacles, size);
+
+    if (foundPath)
+    {
+        lastPossibleIndex = index;
+        from = index + 1;
+    }
+    else
+    {
+        to = index;
+    }
+}
+var result2 = obstacles[lastPossibleIndex];
+Console.WriteLine($"{result2.X},{result2.Y}");
+
+// Dijkstra's Algorithm
+(bool FoundPath, Node EndNode) FindShortestPath(Point start, Point target, Point[] obstacles, int size)
+{
+    var startNode = new Node(start, AccumulatedCost: 0, Parent: null);
+
+    PriorityQueue<Node, int> queue = new([(startNode, startNode.AccumulatedCost)]);
+    Dictionary<string, Node> shortestPaths = new() { [startNode.Key] = startNode };
+
+    while (queue.Count > 0)
+    {
+        var node = queue.Dequeue();
+
+        foreach (var n in node.GetNeighbours(size, obstacles))
+        {
+            if (n.Position == target) return (true, n);
+
+            if (!shortestPaths.ContainsKey(n.Key) || n.AccumulatedCost < shortestPaths[n.Key].AccumulatedCost) // relaxed
+            {
+                shortestPaths[n.Key] = n;
+                queue.Enqueue(n, n.AccumulatedCost);
+            }
+        }
+    }
+
+    return (false, startNode);
+}
+
+record Node(Point Position, int AccumulatedCost, Node? Parent)
+{
+    public string Key => Position.ToString();
+
+    public IEnumerable<Node> GetNeighbours(int size, Point[] obstacles)
+    {
+        List<Point> nextPositions = [Position.Add(0, 1), Position.Add(1, 0), Position.Add(0, -1), Position.Add(-1, 0)];
+        foreach (var nextPosition in nextPositions)
+        {
+            if (IsWithinBounds(nextPosition, size) && !obstacles.Contains(nextPosition))
+            {
+                yield return new Node(nextPosition, AccumulatedCost + 1, this);
+            }
+        }
+    }
+
+    private bool IsWithinBounds(Point p, int size) => p.X >= 0 && p.Y >= 0 && p.Y <= size && p.X <= size;
+}
