faf59a99c1f0ac2cc2f88800196f7954f6999d32
tdor91 <thomas.dorfer@gmx.net>
2024-12-25 19:42:38 +0100

    add solution for day 22 part 1

---
+++ Day22/Program.cs
@@ -0,0 +1,21 @@
+﻿const string inputFile = @"input.txt";
+var lines = await File.ReadAllLinesAsync(inputFile);
+var codes = lines.Select(long.Parse).ToArray();
+
+IEnumerable<long> nextCodes = codes.ToArray();
+for (int i = 0; i < 2000; i++)
+{
+    nextCodes = nextCodes.Select(Next);
+}
+var result1 = nextCodes.Sum();
+Console.WriteLine(result1);
+
+Console.WriteLine("done");
+
+long Next(long n)
+{
+    var a = ((n * 64) ^ n) % 16777216;
+    var b = ((a / 32) ^ a) % 16777216;
+    var c = ((b * 2048) ^ b) % 16777216;
+    return c;
+}
