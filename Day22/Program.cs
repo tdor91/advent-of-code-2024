ed093e9fe45dee238ed2d4f0e5af9df502d2e05d
tdor91 <thomas.dorfer@gmx.net>
2024-12-26 19:12:24 +0100

    add solution for day 22 part 2

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,4 +1,6 @@
-﻿const string inputFile = @"input.txt";
+﻿using Common;
+
+const string inputFile = @"input.txt";
 var lines = await File.ReadAllLinesAsync(inputFile);
 var codes = lines.Select(long.Parse).ToArray();
 
@@ -10,7 +12,32 @@ for (int i = 0; i < 2000; i++)
 var result1 = nextCodes.Sum();
 Console.WriteLine(result1);
 
-Console.WriteLine("done");
+var histories = codes.ToDictionary(c => c, c => new Queue<long>([c]));
+var sequenceProfits = new Dictionary<string, Dictionary<long, long>>();
+for (int i = 0; i < 2000; i++)
+{
+    foreach (var code in codes)
+    {
+        var hist = histories[code];
+        var price = Next(hist.Last());
+        hist.Enqueue(price);
+        
+        if (hist.Count > 5) hist.Dequeue();
+        
+        if (hist.Count == 5)
+        {
+            var seq = string.Join(",", hist.Select(x => x % 10).Pairwise().Select(pair => pair.B - pair.A));
+            if (!sequenceProfits.ContainsKey(seq))
+            {
+                sequenceProfits.Add(seq, new());
+            }
+
+            sequenceProfits[seq].TryAdd(code, price % 10);
+        }
+    }
+}
+var result2 = sequenceProfits.Values.Max(kvp => kvp.Values.Sum());
+Console.WriteLine(result2);
 
 long Next(long n)
 {
