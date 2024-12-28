ca42b99949ed27751a5be05795d946648a99233f
tdor91 <thomas.dorfer@gmx.net>
2024-12-28 19:41:40 +0100

    add solution for day 25 part 1

---
+++ Day25/Program.cs
@@ -0,0 +1,53 @@
+﻿
+const string inputFile = @"input.txt";
+var lines = await File.ReadAllLinesAsync(inputFile);
+var (keys, locks) = ParseInput(lines);
+
+var result1 = 0;
+foreach (var @lock in locks)
+{
+    foreach (var key in keys)
+    {
+        var fit = key.Pattern.Zip(@lock.Pattern).All(z => z.First + z.Second <= 5);
+        if (fit)
+        {
+            result1++;
+        }
+    }
+}
+Console.WriteLine(result1);
+
+
+Console.WriteLine("done");
+
+(List<Key> keys, List<Lock> locks) ParseInput(string[] lines)
+{
+    List<Key> keys = [];
+    List<Lock> locks = [];
+
+    int i = 0;
+    while (i < lines.Length)
+    {
+        var firstLine = lines[i++];
+        int[] pattern = new int[firstLine.Length];
+
+        for (int j = 0; j < 5; j++, i++)
+        {
+            var line = lines[i];
+            for (int k = 0; k < line.Length; k++)
+            {
+                pattern[k] += line[k] == '#' ? 1 : 0;
+            }
+        }
+
+        if (firstLine == "#####") locks.Add(new Lock(pattern));
+        else keys.Add(new Key(pattern));
+
+        i += 2;
+    }
+
+    return (keys, locks);
+}
+
+record Key(int[] Pattern);
+record Lock(int[] Pattern);
